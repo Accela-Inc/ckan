ecbf36d7a07ad22db1ded72c718f2b696f64d022
tanmayAccela <tthakur@accela.com>
2015-08-12 16:43:37 -0700

    changes in datastore controller

--- ckanext/datastore/controller.py
+++ ckanext/datastore/controller.py
@@ -1,7 +1,7 @@
 import StringIO
 import unicodecsv as csv
 
-import pylons
+import pylons 
 
 import ckan.plugins as p
 import ckan.lib.base as base
@@ -17,13 +17,25 @@ class DatastoreController(base.BaseController):
             'session': model.Session,
             'user': p.toolkit.c.user
         }
-
-        data_dict = {
+        
+        # CivicData customization block starts
+        try:
+            from pylons import config
+            data_dict = {
+                'resource_id': resource_id,
+                'limit': request.GET.get('limit', config.get('ckan.limit', '100000')),
+                'offset': request.GET.get('offset', 0),
+                'sort': request.GET.get('sort', '_id ASC')
+            }
+        except Exception, ex:
+            # This is original data_dict for ckan-2.4.0
+            data_dict = {
             'resource_id': resource_id,
             'limit': request.GET.get('limit', 100000),
             'offset': request.GET.get('offset', 0)
-        }
-
+        }    
+        # CivicData customization block ends
+        
         action = p.toolkit.get_action('datastore_search')
         try:
             result = action(context, data_dict)
@@ -42,3 +54,33 @@ class DatastoreController(base.BaseController):
         for record in result['records']:
             wr.writerow([record[column] for column in header])
         return f.getvalue()
+    
+    # CivicData customization block starts
+    def json(self, resource_id):
+        try:
+            from pylons import config
+            from ckan.common import c
+            import urllib2
+            import logging
+            log = logging.getLogger(__name__)
+            
+            f = StringIO.StringIO()
+            sql = 'SELECT%20*%20from%20%22{0}%22%20LIMIT%20{1}'.format(resource_id, config.get('ckan.limit', '100000'))
+            url = config.get('ckan.site_url', '') + '/api/3/action/datastore_search_sql?apikey='+ c.userobj.apikey +'&sql=' + sql
+            log.debug(url)
+            result = urllib2.urlopen(url)
+            while True:
+                s = result.read(1024 * 32)
+                if len(s) == 0:
+                    break
+                f.write(s)
+            pylons.response.headers['Content-Type'] = 'text/json'
+            pylons.response.headers['Content-disposition'] = \
+              'attachment; filename="{name}.json"'.format(name=resource_id)
+            f.flush()
+            return f.getvalue()
+        except p.toolkit.ObjectNotFound:
+            base.abort(404, p.toolkit._('DataStore resource not found'))
+
+        return None
+    # CivicData customization block ends
